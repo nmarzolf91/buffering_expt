5790c6151e47c0b775d28e723e767e522e819d1f
Nick Marzolf <nmarzolf91@gmail.com>
2021-06-04 13:45:42 -0400

    initial commit

---
+++ veganCovEllipse.R
@@ -0,0 +1,5 @@
+veganCovEllipse <- function (cov, center = c(0, 0), scale = 1, npoints = 100) {
+  theta <- (0:npoints) * 2 * pi/npoints
+  Circle <- cbind(cos(theta), sin(theta))
+  t(center + scale * t(Circle %*% chol(cov)))
+}
